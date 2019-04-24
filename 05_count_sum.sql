1d992d8124230314eec68776ba94d09d26c86290
madcido <fabowow@gmail.com>
2019-04-24 20:33:50 -0300

    small fix

--- 05_count_sum.sql
+++ 05_count_sum.sql
@@ -1,21 +1,23 @@
--- question 1
+--question 1
 SELECT SUM(population) FROM world;
 
--- question 2
+--question 2
 SELECT DISTINCT continent FROM world;
 
--- question 3
+--question 3
 SELECT SUM(gdp) FROM world WHERE continent = 'Africa';
 
--- question 4
-SELECT COUNT(name) FROM world WHERE area > 1000000;
+--question 4
+SELECT COUNT(name) FROM world WHERE area >= 1000000;
 
--- question 5
-SELECT SUM(population) from world WHERE name in ('Estonia', 'Latvia', 'Lithuania')
+--question 5
+SELECT SUM(population) from world WHERE name in ('Estonia', 'Latvia', 'Lithuania');
 
--- question 6
-SELECT DISTINCT(continent), COUNT(name) FROM world WHERE population >= 10000000 GROUP BY continent;
+--question 6
+SELECT continent, COUNT(name) FROM world GROUP BY continent;
 
--- question 7
-SELECT DISTINCT(continent) FROM world GROUP BY continent HAVING SUM(population) > 100000000;
+--question 7
+SELECT continent, COUNT(*) FROM world WHERE population >= 10000000 GROUP BY continent;
 
+--question 8
+SELECT continent FROM world GROUP BY continent HAVING SUM(population) >= 100000000;
