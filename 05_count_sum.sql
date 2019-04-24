a84050a9614a8372321c1bff70aa9b1186d00936
Dan <danmontielh@gmail.com>
2019-04-24 16:41:41 -0500

    count sum finished

---
+++ 05_count_sum.sql
@@ -0,0 +1,21 @@
+-- question 1
+SELECT SUM(population) FROM world;
+
+-- question 2
+SELECT DISTINCT continent FROM world;
+
+-- question 3
+SELECT SUM(gdp) FROM world WHERE continent = 'Africa';
+
+-- question 4
+SELECT COUNT(name) FROM world WHERE area > 1000000;
+
+-- question 5
+SELECT SUM(population) from world WHERE name in ('Estonia', 'Latvia', 'Lithuania')
+
+-- question 6
+SELECT DISTINCT(continent), COUNT(name) FROM world WHERE population >= 10000000 GROUP BY continent;
+
+-- question 7
+SELECT DISTINCT(continent) FROM world GROUP BY continent HAVING SUM(population) > 100000000;
+
